1451e75d946495d60ec710c167ea711a8403a8dd
Yi-Wei Chen <feberium@gmail.com>
2019-04-24 00:15:06 -0500

    Update plot.py

--- src/plot.py
+++ src/plot.py
@@ -1,40 +1,64 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-color_seq = [
-    '#1f77b4', '#ff7f0e', '#2ca02c',
-    '#ff9896', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
-    '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'
-]
+color_seq = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
+                  '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
+                  '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
+                  '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+
 if __name__ == '__main__':
-    files = [
-        # '/home/yiwei/enas/0423/acc_micro_final_clean_init_arch',
-        '/home/yiwei/enas/0423/acc_micro_final_clean_final_arch',
-        # '/home/yiwei/enas/0423/acc_micro_final_uniform_noise_08_init_arch',
-        '/home/yiwei/enas/0423/acc_micro_final_uniform_noise_08_final_arch',
-        # '/home/yiwei/enas/0423/acc_micro_final_uniform_noise_08_robust_init_arch_v2',
-        '/home/yiwei/enas/0423/acc_micro_final_uniform_noise_08_robust_final_arch',
-    ]
-    labels = [
-        # 'Clean Search + Init Arch.',
-        'Clean Search + Final Arch.',
-        # 'Noise Search + Init Arch. (0.28)',
-        'Noise Search + Final Arch.',
-        # 'Noise + Robust + Search + Init Arch. (0.28)',
-        'Noise Search + Final Arch + Robust Loss (alpha=1.0)',
-    ]
-    for idx, file_name in enumerate(files):
+    files = {
+        # 'clean_init': '/home/yiwei/mac_book/enas/acc_micro_final_clean_init_arch',
+        # 'clean_final': '/home/yiwei/mac_book/enas/acc_micro_final_clean_final_arch',
+        # 'noise_init': '/home/yiwei/mac_book/enas/acc_micro_final_uniform_noise_08_init_arch',
+        # 'noise_final': '/home/yiwei/mac_book/enas/acc_micro_final_uniform_noise_08_final_arch',
+        # 'robust_init': 'Noise Search + Robust + Init Arch',
+        # 'robust_final': 'Noise Search + Robust + Final Arch',
+        # 'clean_final': '/home/yiwei/mac_book/enas/enas_qq/macro_final_fit/acc_enas_macro_clean_final_fit',
+        # 'noise_init': '/home/yiwei/mac_book/enas/enas_qq/macro_final_fit/acc_enas_macro_permute_init_fit',
+        # 'noise_final': '/home/yiwei/mac_book/enas/enas_qq/macro_final_fit/acc_enas_macro_permute_final_fit',
+        'clean_final': '/home/yiwei/mac_book/enas/auto_keras/acc_clean_7539',
+        # 'noise_final': '/home/yiwei/mac_book/enas/auto_keras/acc_perm_7114',
+        # 'robust_final': '/home/yiwei/mac_book/enas/auto_keras/acc_perm_robust_7321',
+        'noise_final': '/home/yiwei/mac_book/enas/auto_keras/acc_sym_055_7329',
+        'robust_final': '/home/yiwei/mac_book/enas/auto_keras/acc_sym_055_robust_7215',
+
+    }
+    labels = {
+        'clean_init': 'Clean Search + Init Arch',
+        'clean_final': 'Clean Search + Final Arch',
+        'noise_init': 'Noise Search + Init Arch',
+        'noise_final': 'Noise Search + Final Arch',
+        'robust_init': 'Noise Search + Robust + Init Arch',
+        'robust_final': 'Noise Search + Robust + Final Arch',
+    }
+    colors = {
+        'clean_init': color_seq[0],
+        'clean_final': color_seq[2],
+        'noise_init': color_seq[6],
+        'noise_final': color_seq[4],
+        'robust_init': color_seq[8],
+        'robust_final': color_seq[10],
+
+    }
+
+    legend = []
+    for key, file_name in files.iteritems():
         with open(file_name) as f:
             content = f.readlines()
         line_num = len(content)
         y_axis = map(float, content)
         x_axis = np.arange(1, line_num + 1)
-        plt.plot(x_axis, y_axis, color=color_seq[idx], label=labels[idx])
-    plt.legend(labels)
-    plt.title('0423 Uniform Noise (Keep = 0.28)')
+        if len(y_axis) > 300:
+            y_axis = y_axis[:300]
+            x_axis = x_axis[:300]
+        plt.plot(x_axis, y_axis, color=colors[key], label=labels[key])
+        legend.append(labels[key])
+
+    plt.legend(legend)
+    plt.title('Uniform Noise 0.5 (Auto-Keras)')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
-    plt.savefig('enas_micro_final_train_uniform_08')
+    plt.savefig('auto_keras_sym_055_uni_05')
     # plt.show()
-
 
